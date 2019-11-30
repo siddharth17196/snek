6c07a1e4f35ea8d76d105a9b21ce8077a5e003eb
Aniket Pradhan <aniket17133@iiitd.ac.in>
2019-11-30 13:43:04 +0530

    Added inceptionv3

---
+++ scripts/inception_baseline.py
@@ -0,0 +1,227 @@
+import os
+import copy
+import random
+import argparse
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import f1_score
+
+import torch
+import torchvision
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.utils.checkpoint as cp
+from torchvision import datasets, models, transforms
+
+def load_train_dataset(root, batchsize, input_size, crop_size):
+    data_path = root
+    trainTransform  = torchvision.transforms.Compose([torchvision.transforms.Resize((input_size, input_size)),
+                        torchvision.transforms.CenterCrop(crop_size),
+                        torchvision.transforms.ToTensor(),
+                        transforms.Normalize([0.0432, 0.0554, 0.0264], [0.8338, 0.8123, 0.7803]),
+                        ])
+    train_dataset = torchvision.datasets.ImageFolder(
+        root=data_path,
+        transform=trainTransform
+    )
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=batchsize,
+        num_workers=4,
+        shuffle=True
+    )
+    return train_loader, train_dataset
+
+def set_parameter_requires_grad(model, feature_extracting):
+    if feature_extracting:
+        for param in model.parameters():
+            param.requires_grad = False
+            
+def initialize_model(model_name, num_classes, feature_extract, use_pretrained=True):
+    model_ft = None
+    input_size = 0
+
+    if model_name == "resnet":  # ResNet-50
+        model_ft = models.resnet50(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+        crop_size = 224
+
+    elif model_name == "vgg":  # VGG-11
+        model_ft = models.vgg11_bn(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier[6].in_features
+        model_ft.classifier[6] = nn.Linear(num_ftrs,num_classes)
+        input_size = 224
+        crop_size = 224
+
+    elif model_name == "densenet":  # DenseNet-121
+        model_ft = models.densenet121(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier.in_features
+        model_ft.classifier = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+        crop_size = 224
+    
+    elif model_name == "inception":
+        model_ft = models.inception_v3(pretrained=use_pretrained, aux_logits=False)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs, num_classes)
+        input_size = 299
+        crop_size = 299
+
+    return model_ft, input_size, crop_size
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--traindir', required=True)
+parser.add_argument('-v', '--validdir', required=True)
+parser.add_argument('-m', '--modeldir', required=False, default="models/")
+parser.add_argument('-b', '--batchsize', required=False, default=32)
+
+args = parser.parse_args()
+train_data_dir = args.traindir
+valid_data_dir = args.validdir
+model_dir = args.modeldir
+
+if model_dir[-1] != "/":
+    model_dir += "/"
+
+train_data = datasets.ImageFolder(train_data_dir)
+num_classes = len(train_data.classes)
+model_name = "inception"  # resnet, vgg, densenet, inception
+batch_size = int(args.batchsize)
+feature_extract = False
+
+image_datasets = {}
+dataloaders_dict = {}
+
+model_ft, input_size, crop_size = initialize_model(model_name, num_classes, feature_extract, use_pretrained=True)
+
+dataloaders_dict['train'], image_datasets['train'] = load_train_dataset(train_data_dir, batch_size, input_size, crop_size)
+dataloaders_dict['valid'], image_datasets['valid'] = load_train_dataset(valid_data_dir, batch_size, input_size, crop_size)
+class_names = image_datasets['train'].classes
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+def checkandcreatedir(path):
+	if not os.path.isdir(path):
+		os.makedirs(path)
+
+def train_model(model, dataloaders, criterion, optimizer, model_name, model_dir, num_epochs=25):
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_fscore = 0.0
+    
+    loss_train_evo=[]
+    acc_train_evo=[]
+    fs_train_evo=[]
+    
+    loss_val_evo=[]
+    acc_val_evo=[]
+    fs_val_evo=[]
+    
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch+1, num_epochs))
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'valid']:
+            print("phase:", phase)
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+            
+            running_loss = 0.0
+            running_corrects = 0
+            fscore = []
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+                optimizer.zero_grad()
+
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    loss = criterion(outputs, labels)
+                    _, preds = torch.max(outputs, 1)
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                labels_cpu = labels.cpu().numpy()
+                predictions_cpu = preds.cpu().numpy()
+                Fscore = f1_score(labels_cpu, predictions_cpu, average='macro')
+                fscore.append(Fscore)
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+            epoch_fscore = np.average(np.array(fscore))
+            
+            print('{} Loss: {:.4f} Acc: {:.4f} F: {:.3f}'.format(phase, epoch_loss, epoch_acc, epoch_fscore))
+
+            checkandcreatedir(model_dir)
+            torch.save(model.state_dict(), model_dir + model_name + "_model"+str(epoch))
+            torch.save(optimizer.state_dict(), model_dir + model_name + "_optim"+str(epoch))
+            
+            if phase == 'train':
+                loss_train_evo.append(epoch_loss)
+                epoch_acc = epoch_acc.cpu().numpy()
+                acc_train_evo.append(epoch_acc)
+                fs_train_evo.append(epoch_fscore)                
+            else:
+                loss_val_evo.append(epoch_loss)
+                epoch_acc = epoch_acc.cpu().numpy()
+                acc_val_evo.append(epoch_acc)
+                fs_val_evo.append(epoch_fscore) 
+                
+            # deep copy the model
+            if phase == 'valid' and epoch_fscore > best_fscore:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+    model.load_state_dict(best_model_wts)
+    return model, loss_train_evo, acc_train_evo, fs_train_evo, loss_val_evo, acc_val_evo, fs_val_evo
+
+model_ft = model_ft.to(device)
+
+params_to_update = model_ft.parameters()
+if feature_extract:
+    params_to_update = []
+    for name,param in model_ft.named_parameters():
+        if param.requires_grad == True:
+            params_to_update.append(param)
+else:
+    for name,param in model_ft.named_parameters():
+        if param.requires_grad == True:
+            a=1
+
+# Optimizer
+optimizer_ft = optim.Adam(params_to_update, lr=3e-4)
+
+# Loss Funciton
+criterion = nn.CrossEntropyLoss()
+
+num_epochs = 20
+model_ft, loss_train, acc_train, fs_train, loss_val, acc_val, fs_val = train_model(model_ft, dataloaders_dict, criterion, optimizer_ft, model_name, model_dir, num_epochs=num_epochs)
+
+def plot_metric(metric_train, metric_val, title):
+    fig, (ax) = plt.subplots(1, 1)
+    fig.suptitle(title)
+    ax.set(xlabel='epoch')
+    ax.plot(metric_train, label='Training')
+    ax.plot(metric_val, label='Validation')
+    ax.legend(loc='upper left')
+    plt.savefig(title + "_metric.png")
+
+plot_metric(loss_train, loss_val, 'Loss')
+plot_metric(acc_train, acc_val, 'Accuracy')
+plot_metric(fs_train, fs_val, 'F-Score')
