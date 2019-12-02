3462275fa893e7aa5d2196193f8e46d6b9886e70
Aniket Pradhan <aniket17133@iiitd.ac.in>
2019-12-02 18:51:31 +0530

    clean preds

--- scripts/getpreds.py
+++ scripts/getpreds.py
@@ -4,6 +4,7 @@ import random
 import matplotlib.pyplot as plt
 import os
 import copy
+import argparse
 import pickle
 import torch.optim as optim
 import numpy as np
@@ -93,22 +94,21 @@ args = parser.parse_args()
 data_dir = args.datadir
 model_name = args.modelname
 model_dir = args.model
-batch_size = args.batchsize
+batch_size = int(args.batchsize)
 
 data = datasets.ImageFolder(data_dir)
 num_classes = len(data.classes)
 
 feature_extract = False
 
-torch.cuda.set_device(0)
+# torch.cuda.set_device(0)
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print(torch.cuda.current_device())
+# print(torch.cuda.current_device())
 
 model_ft, input_size, crop_size = initialize_model(model_name, num_classes, feature_extract, use_pretrained=True)
-loader, dataset = load_train_dataset("./datasets/valid", batch_size, input_size, crop_size)
+loader, dataset = load_train_dataset(data_dir, batch_size, input_size, crop_size)
 
-model_weights = torch.load(model_dir)
-print(model_weights)
+model_weights = torch.load(model_dir, map_location=device)
 model_ft.load_state_dict(model_weights)
 print('Loaded model', model_dir)
 model_ft = model_ft.to(device)
@@ -128,7 +128,6 @@ for i, (images, labels) in enumerate(loader):
 
 	output = model_ft(images)
 	_, preds = torch.max(output, 1)
-	print(preds)
 	# class_id = preds.item()
 	y_pred.extend(preds)
 	# print("Predicted class: ", y_pred, y_test)
@@ -140,47 +139,3 @@ f.close
 f = open('tests_' + model_name + '.pkl', 'wb')
 pickle.dump(y_test, f)
 f.close
-
-exit()
-
-# def get_item(di):
-# 	global flag
-# 	ima_dir = os.path.join(di)
-# 	scaler = transforms.Resize((224, 224))
-# 	normalize = transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
-# 	to_tensor = transforms.ToTensor()
-# 	try:
-# 		ima = Image.open(ima_dir)
-# 		plt.imshow(ima)
-# 		item = (normalize(to_tensor(scaler(ima))).unsqueeze(0)).to(device)
-# 		return item
-# 	except:
-# 		flag=0
-# 		return 0
-
-# s = './datasets/valid/'
-# model_ft.eval()
-# y_pred=[]
-# y_test=[]
-# for files in os.listdir('./datasets/valid/'):
-# 	f = os.listdir(s+files)
-# 	for i in range(len(f)):
-# 		flag=1
-# 		y_test.append(files)
-# 		item = get_item(s+files+'/'+f[i])
-# 		if flag==0:
-# 			y_pred.append(files)
-# 		else:
-# 			output = model_ft(item)
-# 			_, preds = torch.max(output, 1)
-# 			class_id = preds.item()
-# 			y_pred.append(class_names[class_id])
-# 			print("Predicted class: ", class_names[class_id])
-
-# f = open('predsmod11.pkl', 'wb')
-# pickle.dump(y_pred, f)
-# f.close
-
-# f = open('testmod11.pkl', 'wb')
-# pickle.dump(y_test, f)
-# f.close
