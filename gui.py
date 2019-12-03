fb49151a41812b0459c9720f6092e071e9887478
Aniket Pradhan <aniket17133@iiitd.ac.in>
2019-12-03 08:49:24 +0530

    Updated gui a bit

--- gui.py
+++ gui.py
@@ -72,6 +72,7 @@ class Model:
 	        self.crop_size = 224
 
 	def get_model_name(self):
+		print(self.modeldir)
 		if "resnext" in self.modeldir.lower():
 			self.modelname = "resnext"
 		elif "inception" in self.modeldir.lower():
@@ -79,7 +80,8 @@ class Model:
 		elif "densenet" in self.modeldir.lower():
 			self.modelname = "densenet"
 		else:
-			raise Exception("Cannot load the current model")
+			return False
+		return True
 
 	def load_weights(self):
 		model_weights = torch.load(self.modeldir, map_location = self.device)
@@ -96,7 +98,8 @@ class Model:
 	    return image
 
 	def load_classes(self):
-		self.classes = datasets.ImageFolder(self.classes).classes
+		# self.classes = datasets.ImageFolder(self.classes).classes
+		self.classes = ['Thamnophis Proximus', 'Nerodia Sipedon', 'Opheodrys Vernalis', 'Crotalus Horridus', 'Crotalus Pyrrhus', 'Nerodia Rhombifer', 'Thamnophis Sirtalis', 'Natrix Natrix', 'Crotalus Adamanteus', 'Charina Bottae', 'Pituophis Catenifer', 'Lampropeltis Triangulum', 'Nerodia Erythrogaster', 'Thamnophis Marcianus', 'Lampropeltis Californiae', 'Crotalus Ruber', 'Rhinocheilus Lecontei', 'Opheodrys Aestivus', 'Thamnophis Ordinoides', 'Thamnophis Radix', 'Masticophis Flagellum', 'Pantherophis Vulpinus', 'Hierophis Viridiflavus', 'Feterodon Platirhinos', 'Pantherophis Emoryi', 'Regina Septemvittata', 'Haldea Striatula', 'Diadophis Punctatus', 'Nerodia Fasciata', 'Storeria Occipitomaculata', 'Crotalus Scutulatus', 'Storeria Dekayi', 'Crotalus Viridis', 'Boa Imperator', 'Pantherophis Obsoletus', 'Lichanura Trivirgata', 'Agkistrodon Contortrix', 'Thamnophis Elegans', 'Agkistrodon Piscivorus', 'Pantherophis Guttatus', 'Crotalus Atrox', 'Carphophism Amoenus', 'Coluber Constrictor', 'Pantherophis Spiloides', 'Pantherophis Alleghaniensis']
 		print(type(self.classes))
 
 	def predict(self, image_dir):
@@ -108,7 +111,7 @@ class Model:
 		pred = self.model_ft(self.single_image_loader(data_transforms, image_dir))
 		_, pred = torch.max(pred, 1)
 		try:
-			return "The class label is: " + str(self.classes[pred])
+			return "The class label is: " + str(self.classes[pred]) + ".\nClass index is: " + str(pred)
 		except:
 			return "Incomplete dataset. The class index is: " + str(pred)
 
@@ -118,28 +121,46 @@ class Model:
 		self.num_classes = 45
 		self.feature_extract = False
 		self.classes = None
+		self.model_ft = None
 
-		self.get_model_name()
-		self.initialize_model()
-		print("Loaded " + self.modelname + " model")
-		self.load_weights()
+		got_model_name = self.get_model_name()
+		print(got_model_name)
+		if got_model_name:
+			self.initialize_model()
+			print("Loaded " + self.modelname + " model")
+			self.load_weights()
+		else:
+			return
 		
 
 class MainWindow(QtWidgets.QMainWindow):
 	
 	def SelectModel(self):
-		modelname = QtWidgets.QFileDialog.getOpenFileName()
-		self.model = Model(modelname[0])
+		modelname = QtWidgets.QFileDialog.getOpenFileName()[0]
+		if modelname == "":
+			return
+		print(modelname)
+		self.model = Model(modelname)
+		if self.model.model_ft == None:
+			self.outLabel.setText("Cannot load model")
+			return
+		else:
+			self.outLabel.setText("Loaded model")
 		## Load classes
-		self.model.classes = QtWidgets.QFileDialog.getExistingDirectory(self, "Select Directory")
+		# self.model.classes = QtWidgets.QFileDialog.getExistingDirectory(self, "Select Directory")
 		self.model.load_classes()
 		if self.image != None:
 			self.predict.setStyleSheet("background-color: green; font: white")
 
 	def SelectImage(self):
-		imagename = QtWidgets.QFileDialog.getOpenFileName()
-		self.image = imagename[0]
+		imagename = QtWidgets.QFileDialog.getOpenFileName()[0]
+		self.image = imagename
+		if self.image == "":
+			return
 		print("Image loaded")
+		print(self.image)
+		imagename = imagename.split("/")[-1]
+		self.outLabel.setText("Loaded image: " + imagename)
 		if self.model != None:
 			self.predict.setStyleSheet("background-color: green; font: white")
 
